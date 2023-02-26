cd60bd9a9bc9f038199dcc1354539b984fad7816
Montanher96 <gabrielmontanherr@gmail.com>
2023-02-26 15:20:20 -0300

    updates

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.aside`
+font-family: Arial, Helvetica, sans-serif;
+width: 500px;
+padding: 20px;
+
+
+.buy {
+    color: blue;
+    background: #cdcdcd;
+    display: flex;
+    border: 0;
+    padding: 10px;
+    width: 200px;
+    height: 50px;
+    display: inline-block;
+    margin: 10px;
+    cursor: pointer;
+    border-radius: 4px;
+}
+
+`;
